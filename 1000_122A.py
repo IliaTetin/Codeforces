d01c43384e6046d3ba79efb81b88aa92d3e9a1ad
IliaTetin <121325844+IliaTetin@users.noreply.github.com>
2024-09-26 09:47:19 +0800

    Update 1000_122A.py

--- 1000_122A.py
+++ 1000_122A.py
@@ -1,5 +1,5 @@
 n = int(input())
-lucky = [4,7,44,47,74,77, 447,474, 477, 744,747,777]
+lucky = [4, 7, 44, 47, 74, 77, 447, 474, 477, 744, 747, 777]
 flag = False
 for i in range(len(lucky)):
     if n % lucky[i] == 0:
