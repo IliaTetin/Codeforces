da5f9373dcb2e44bbcb275d143d2d313b6ee0b2e
IliaTetin <121325844+IliaTetin@users.noreply.github.com>
2024-09-28 09:44:16 +0800

    Update 479B.py

--- 479B.py
+++ 479B.py
@@ -1,28 +1,25 @@
 n, k = map(int, input().split())
 h = list(map(int, input().split()))
-
-maxdif = max(h) - min(h)
+dif = max(h) - min(h)
 out_cnt = 0
 out = []
-while maxdif != 0 or k != 0:
-    curmax = max(h)
-    curmaxind = h.index(max(h))
-    curmin = min(h)
-    curminind = h.index(min(h))
-    out_cnt += 1
-    out.append([curmaxind + 1, curminind + 1])
 
+while True:
+    curmaxind = h.index(max(h))
+    curminind = h.index(min(h))
+  
+    if h[curmaxind] - h[curminind] <= 1:
+        break
+    out_cnt += 1
     h[curmaxind] -= 1
     h[curminind] += 1
     k -= 1
-    newdif = maxdif - (max(h) - min(h))
-    if newdif == 0:
-        #out_cnt -= 1
-        #out.pop()
+    out.append([curmaxind + 1, curminind + 1])
+    dif = max(h) - min(h)        
+    if k == 0:
         break
-    else:
-        maxdif = max(h) - min(h)
-print(maxdif, out_cnt)
+    if dif == 0:
+        break
+print(dif, out_cnt)
 for i, val in enumerate(out):
     print(*val)
-
