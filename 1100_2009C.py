1c22c9be044041a1de01cecb90113f2fb9817c32
IliaTetin <121325844+IliaTetin@users.noreply.github.com>
2024-09-29 10:19:24 +0800

    Create 1100_2009C.py

---
+++ 1100_2009C.py
@@ -0,0 +1,9 @@
+import math
+
+t = int(input())
+for _ in range(t):
+    x,y,k = map(int, input().split())
+    if math.ceil(x/k) > math.ceil(y/k):
+        print(2*math.ceil(x/k)-1)
+    else:
+        print(2*math.ceil(y/k))
